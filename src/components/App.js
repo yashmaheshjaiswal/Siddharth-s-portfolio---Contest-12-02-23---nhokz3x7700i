127232b96d7457dca265bbf9cd1d6101cb7fef7e
Yash Jaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-02-12 13:55:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
 // import { BrowserRouter as Router, Route,Link,Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
 import Info from './Info';
 import Blog from './Blog';
 import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
 const App = () => {
 
   return (
     <div id="main">
 
 
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <route path='/blog' exact>
-            <Blog />
-          </route>
-          <route path='/info' exact>
-            <Info />
-          </route>
-        </Switch>
-      </Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path='/blog' exact element={<Blog />} />
+        <Route path='/info' exact element={<Info />} />
+
+      </Routes>
     </div>
   )
 }
